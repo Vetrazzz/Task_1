6321d50d4728f5948876d3ce49cc2c28e063f735
Ilya Glazunov <vetraz@inbox.ru>
2015-10-10 00:27:23 +0300

    dirty ver

---
+++ app/slider.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){    
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 15,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: false,
+  arrows: false,
+  focusOnSelect: true,
+  infinite:false
+});
+    });
